663a8dbc044d45d9f1990b02bfa3eb02d749044b
Thomas Burton <tomjohnburton@gmail.com>
2021-12-22 16:03:27 +0100

    Remove sensitive info

--- debug.sh
+++ debug.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# Start in $HOME directory
+cd
+
 # Create a new debug log
 debugLog=/tmp/debug.log
 echo "-------------- $(date) --------------" > $debugLog
@@ -8,8 +11,9 @@ function printDivider() {
   echo "-------------------------------" >> $debugLog
 }
 # Find the log files for the engine and node
-nodeLog=$(find . -name "node.log")
-engineLog=$(find . -name "engine.log")
+nodeLog=$(find . -name "node.log" | head -1)
+engineLog=$(find . -name "engine.log" | head -1)
+engineConfig=$(find . -name "Default.toml" | head -1)
 
 # Find the system files for the engine and node
 cat /etc/systemd/system/chainflip-engine.service >> $debugLog
@@ -22,3 +26,6 @@ tail -100 $nodeLog >> $debugLog
 printDivider
 tail -100 $engineLog >> $debugLog
 printDivider
+
+# Print engine config but delete sensitive information
+cat $engineConfig  | grep -v node_endpoint
