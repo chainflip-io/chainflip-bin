5ffbd1f46d30c82ee263a1b9cc822ee0a28601fa
Thomas Burton <tomjohnburton@gmail.com>
2021-12-22 16:57:51 +0100

    add file names to top

--- debug.sh
+++ debug.sh
@@ -8,7 +8,8 @@ debugLog=/tmp/debug.log
 echo "-------------- $(date) --------------" > $debugLog
 
 function printDivider() {
-  echo "-------------------------------" >> $debugLog
+  local file=$1
+  echo "--------------$file-----------------" >> $debugLog
 }
 # Find the log files for the engine and node
 nodeLog=$(find . -name "node.log" | head -1)
@@ -16,18 +17,22 @@ engineLog=$(find . -name "engine.log" | head -1)
 engineConfig=$(find . -name "*.toml" | head -1)
 
 # Find the system files for the engine and node
-cat /etc/systemd/system/chainflip-engine.service >> $debugLog
-printDivider
+printDivider /etc/systemd/system/chainflip-node.service
 cat /etc/systemd/system/chainflip-node.service >> $debugLog
-printDivider
+
+printDivider /etc/systemd/system/chainflip-engine.service
+cat /etc/systemd/system/chainflip-engine.service >> $debugLog
 
 # Tail the files and output to the debug log
+printDivider $nodeLog
 tail -n 100 $nodeLog >> $debugLog
-printDivider
+
+printDivider $engineLog
 tail -n 100 $engineLog >> $debugLog
 printDivider
 
 # Print engine config but delete sensitive information
+printDivider $engineConfig
 cat $engineConfig  | grep -v node_endpoint >> $debugLog
 
 echo "Please send the contents of $debugLog to a Chainflip Labs member in Discord."
