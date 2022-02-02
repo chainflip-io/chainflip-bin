c5386b3d626ab4866b938a4f40bc21d3bb21c941
Thomas Burton <tomjohnburton@gmail.com>
2022-02-02 11:48:13 +0100

    Branch with just bins

--- scripts/move.sh
+++
@@ -1,3 +0,0 @@
-mv ~/Downloads/chainflip-engine .
-mv ~/Downloads/chainflip-node .
-mv ~/Downloads/chainflip-cli .
--- scripts/release.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-dir=chainflip-$1
-mkdir $dir
-cp bin/* $dir
-tar -czvf chainflip.tar.gz $dir
